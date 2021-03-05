337320662fafa052ed16531f60c8f355586189e0
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-03-06 00:43:01 +0600

    navigate dynamic route

--- src/components/FriendDetail/FriendDetail.js
+++ src/components/FriendDetail/FriendDetail.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
-const FriendDetail = () => {
+const FriendDetail = (props) => {
   const { friendId } = useParams();
+  const [ friend, setFriend ] = useState({});
+  useEffect (() => {
+    const url =`https://jsonplaceholder.typicode.com/users/${friendId}`;
+    fetch(url)
+    .then((response) => response.json())
+    .then(data => setFriend(data))
+  }, [])
+  // const {name, email } = props.friend;
+
   return (
     <div>
-      <h3>This is friend detail Component: {friendId}</h3>
+      <i>This is friend detail Component: {friendId}</i>
+      <h3>Name: {friend.name}</h3>
+      <p>E-mail: {friend.email}</p>
+      <p>Phone: {friend.phone}</p>
+      <p>Website: {friend.website}</p>
     </div>
   );
 };

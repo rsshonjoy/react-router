01e05f46aae4930324c88ba679258873180abbd5
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-03-05 23:31:36 +0600

    add router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import { useEffect, useState } from 'react/cjs/react.development';
+import React from 'react';
 import './App.css';
-import Friend from './components/Friend/Friend';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import NoMatch from './components/Nomatch/NoMatch';
 
 function App() {
-  const [friends, setFriend] = useState([]);
   
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response) =>response.json())
-    .then(data =>setFriend(data))
-  }, [])
   return (
-    <div>
-      <h1>Friends: {friends.length}</h1>
-      {
-        friends.map((friend) => <Friend friend={friend}></Friend>)
-      }
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/Home">
+          <Home></Home>
+        </Route>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+        <Route path="/*">
+          <NoMatch />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
